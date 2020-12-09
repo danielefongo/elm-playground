b9786ae37a98aace35ec575f7e91888c95ac81ef
danielefongo <danielefongo@gmail.com>
2020-12-09 13:54:58 +0100

    add formatter

--- tests/CounterTest.elm
+++ tests/CounterTest.elm
@@ -1,36 +1,37 @@
 module CounterTest exposing (..)
 
+import Browser exposing (sandbox)
+import Counter
+import ProgramTest exposing (clickButton, expectViewHas, start)
 import Test exposing (..)
 import Test.Html.Selector exposing (class, text)
-import ProgramTest exposing (clickButton, expectViewHas, start)
-import Counter
-import Browser exposing (sandbox)
+
 
 suide : Test
 suide =
-  describe "Counter"
-    [ test "default value" <|
-      \() ->
-          setup |> expectViewHas [ text "0" ]
+    describe "Counter"
+        [ test "default value" <|
+            \() ->
+                setup |> expectViewHas [ text "0" ]
+        , test "increment value" <|
+            \() ->
+                setup
+                    |> clickButton "+"
+                    |> clickButton "+"
+                    |> expectViewHas [ text "2" ]
+        , test "decrement value" <|
+            \() ->
+                setup
+                    |> clickButton "-"
+                    |> clickButton "-"
+                    |> expectViewHas [ text "-2" ]
+        ]
 
-    , test "increment value" <|
-      \() ->
-          setup
-            |> clickButton "+"
-            |> clickButton "+"
-            |> expectViewHas [ text "2" ]
 
-    , test "decrement value" <|
-      \() ->
-          setup
-            |> clickButton "-"
-            |> clickButton "-"
-            |> expectViewHas [ text "-2" ]
-  ]
-
-setup = ProgramTest.createSandbox
-  { init = Counter.init
-  , update = Counter.update
-  , view = Counter.view
-  } |> start ()
-
+setup =
+    ProgramTest.createSandbox
+        { init = Counter.init
+        , update = Counter.update
+        , view = Counter.view
+        }
+        |> start ()
